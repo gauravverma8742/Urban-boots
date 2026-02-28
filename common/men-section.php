a82fb24af51222a4e80a8b7c21b71de588361646
mukul chandela <mukulchandela2005@gmail.com>
2026-02-28 22:53:34 +0530

    added new sections

---
+++ common/men-section.php
@@ -0,0 +1,42 @@
+<div class="men">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 text-center">
+                <h3>Men's Section</h3>
+                <p>All you need is these!</p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-4">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="2000">
+                        <img src="images/4.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                        <img src="images/5.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                        <img src="images/6.png" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            <div class="col-xl-8 text-center">
+                <h4>Branded shoes for a gentelmen</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga enim quo recusandae eos aliquid quae fugit animi eligendi aspernatur cupiditate?</p>
+                <button class="btn">Let's go</button>
+            </div>
+        </div>
+    </div>
+</div>
