ecd59e6e00daae4a1edbb11d20a87f359ef4bfed
mukul chandela <mukulchandela2005@gmail.com>
2026-02-28 11:38:24 +0530

    changes

--- common/navigation.php
+++ common/navigation.php
@@ -1,36 +1,38 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<div class="navigation">
+    <div class="container">
+        <div class="row align-item-center">
+            <div class="col-xl-3">
+                <img src="images/1.png" alt="image not found" width="209px">
+            </div>
+            <div class="col-xl-7">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Nike</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Redtape</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Puma</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">About</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <div class="col-xl-2">
+                <button class="cart align-self-center scale"><i class="fa-solid fa-cart-shopping p-1 me-1"></i><span class="zero"><img src="images/2.png" alt="" width="33px"></span></button>
+            </div>
+        </div>
     </div>
-  </div>
-</nav>
+</div>
