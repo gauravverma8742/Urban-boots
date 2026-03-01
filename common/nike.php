3619e6b5d989b38a0b92e45bc12cf6988bc02e5c
Sanjeev Sagar <sanjeevsagar6002@gmail.com>
2026-03-01 11:43:28 +0530

    add nike2

--- common/nike.php
+++ common/nike.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NIKE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
-<?php include('header.php');?>
-<?php include('navigation.php');?>
+
   <div class="mybanner">
     <div class="container-fluid">
         <div class="row">
@@ -91,8 +81,4 @@
     </div>
   </div>
 
-    <?php include('footer.php');?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
-</html>
+    
