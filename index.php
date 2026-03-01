f51fff3bcf1aa2b0f102e9feb80cf2bc186f3324
mukul chandela <mukulchandela2005@gmail.com>
2026-03-01 10:30:20 +0530

    correectimg changes

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include('common/header.php');?>
-<?php include('common/navigation.php');?>
-<?php include('common/banner.php');?>
-<?php include('common/brands.php');?>
-<?php include('common/men-section.php');?>
-<?php include('common/women-section.php');?>
-<?php include('common/footer.php');?>
