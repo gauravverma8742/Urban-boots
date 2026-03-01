6b101e9d900196a82dec708af5ccbf8e64cd0988
mukul chandela <mukulchandela2005@gmail.com>
2026-03-01 10:44:35 +0530

    index file added again

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('common/header.php');?>
+<?php include('common/navigation.php');?>
+<?php include('common/banner.php');?>
+<?php include('common/brands.php');?>
+<?php include('common/men-section.php');?>
+<?php include('common/women-section.php');?>
+<?php include('common/gaurav-footer.php');?>
