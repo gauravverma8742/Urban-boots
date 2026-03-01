c91af6997b5820c04afaad688b9dddc0b364991e
gaurav <gaurav.verma8742@gmail.com>
2026-03-01 10:44:52 +0530

    gaurav added

---
+++ common/gaurav-puma.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Puma</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="gaurav-puma.css" rel="stylesheet">
+  </head>
+  <body>
+
+   <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="images/BANNER-PUMA.webp" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="images/ANUSHKA.jpg" class="d-block w-100" alt="..." style="height: 570px;">
+        </div>
+        <div class="carousel-item">
+        <img src="images/puma.jpg" class="d-block w-100" alt="..." style="height: 570px;">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+    <div class="card2" style="margin-top: 50px; background-color:cornsilk;">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/shoes3.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: 700;">Puma shoes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                <a href="#" class="btn btn-primary"  style="padding:5px 87px;">Order Now</a>
+                            </div>
+                    </div>
+                 </div>
+
+
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/shoes1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: 700;">Puma shoes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                <a href="#" class="btn btn-primary"  style="padding:5px 87px;">Order Now</a>
+                            </div>
+                    </div>
+                 </div>
+
+                 <style>
+                    .card img{height: 220px;}
+                    .card:hover{  box-shadow:0px 0px 10px 3px #9e9e9eb7 ,-0px -0px 10px 8px #9e9e9eb7; 
+                      transition:.5s;}
+                 </style>
+
+
+                <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/shoes2.webp" class="card-img-top" alt="..." >
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: 700;">Puma shoes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                <a href="#" class="btn btn-primary"  style="padding:5px 87px;">Order Now</a>
+                            </div>
+                    </div>
+                 </div>
+
+
+                 <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="images/1.avif" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: 700;">Puma shoes</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                <a href="#" class="btn btn-primary" style="padding:5px 87px;">Order Now</a>
+                            </div>
+                    </div>
+                 </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <style>
+        .b{background: #ef615f; padding: 10px 15px; border-radius: 30px; width: 140px;text-align: center;font-weight: 700; color: white;}
+        .b:hover{background-color: #e06010fc;}        
+    </style>
+     <div class="header" style="margin-top: 80px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="puma"  style=" padding: 10px 20px;">
+                        <p style="color: rgb(244, 99, 99);">Our Summer collections</p>
+                        <h1 style="font-weight: 700; font-size: 55px;">The New Arrival<br><span style="color: rgb(244, 134, 134);">Puma</span> Shoes</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Itaque veniam, quibusdam amet fugiat.</p>
+                        <p class="b">Shop now</p>
+                    </div>
+
+                </div>
+                
+                
+                <div class="col-6">
+                    <div class="puma2">
+                        <img src="images/puma2.jpg" style="height:500px; margin-left: 50px; margin-left: 55pxs;">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+     </div>
+
+
+     <div class="header5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                <div class="footer">   
+                <h2 style="color:#2f2d2dbf;font-size: 35px;">Puma</h2>
+                <p>Empowering students throught transformative
+                    events<br>and experiences at Asian school of Business
+                </p>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-youtube"></i>
+                <i class="fa-brands fa-twitter"></i>
+
+                </div>
+        </div>
+                                                                                      
+            <div class="col-2">
+                <div class="footer">
+                    <h2>Products</h2>
+                    <p>Air force 1<br>Air Max 1<br>Air Jorden 1<br>Air Huarache<br>Puma Waffle Racer<br>Puma Cortez</p>
+
+                </div>
+            </div>
+
+                    <div class="col-2">
+                        <div class="footer">
+                        <h2>Help</h2>
+                        <p>About us<br>FAQs<br>How it works<br>Privacy policy
+                         <br>Payment policy</p>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="footer1">
+                    <h2>Get in touch</h2>
+                    <p>Pumashoes565@gmail.com<br>+919875641352 </p>
+            </div>
+         </div>
+
+    </div>
+</div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
